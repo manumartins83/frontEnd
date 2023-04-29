bde6d1a8056f6a016fe6482505cc61b68b7e5ed8
manumartins83 <manumartins83@hotmail.fr>
2023-04-29 16:43:20 +0200

    DWWM_frontExos_19042023_1640

--- 01-Module1_FrontEnd/02-Exercices/03-JavaScript/ECF2-BD/js/modeleBD.js
+++ 01-Module1_FrontEnd/02-Exercices/03-JavaScript/ECF2-BD/js/modeleBD.js
@@ -9,6 +9,20 @@ var btnRadioTitle = document.getElementById('title'); //sélection bouton radio 
 var btnRadioSerie = document.getElementById('serie'); //sélection bouton radio série album BD
 var selectListSearch = document.getElementById('searchBd'); //sélection liste recherche album BD
 var quantitePanier = document.getElementById('quantite'); //sélection input quantité panier achat
+var titreListPanierAchat = document.getElementById('panierList'); //sélection titre liste panier achat BD
+let a = 0; //initialisation quantité départ input panier achat
+
+
+
+
+var optionSelectSearch = document.getElementById('selection'); //sélection balise "option" liste recherche album BD
+
+
+
+
+
+
+
 
 
 //Chargement et affichage auto albums BD via fonction "showListBd"
@@ -27,6 +41,9 @@ btnRadioSerie.addEventListener("click", function () { showListBdSerie() });
 
 //Création liste albums BD à partir fichier data "albums, series, auteurs"
 function showAlbumsListBd() {
+	//Retrait affichage titre liste panier achat 
+	titreListPanierAchat.style.display = "none";
+
 	//Récupération liste albums BD
 	albums.forEach(album => {
 		//Sélection fichier data "séries et auteurs"
@@ -36,7 +53,7 @@ function showAlbumsListBd() {
 
 		//Variables locales
 		var key = keys.next().value; //sélection numéro clef dans fichier "albums.js"
-		// console.log(key);
+		// console.log(key); //affiche dev
 		var elementListBd = document.getElementById('comicsBD'); //sélection id balise "div" HTML "Recherche BD"
 		var bdCard = document.createElement('div'); //création balise "div" pour card album BD
 		const nomPhotoBd = serie.nom + "-" + album.numero + "-" + album.titre; //récupération nom fichier photo album BD
@@ -49,6 +66,10 @@ function showAlbumsListBd() {
 		//Création attribu class "card" pour album BD
 		bdCard.setAttribute('class', 'card');
 
+
+
+
+
 		//Insertion images dans balise "div" class "card"
 		//si clef album existant alors
 		if (key != false) {
@@ -60,6 +81,10 @@ function showAlbumsListBd() {
 			imgsBd[i].addEventListener("error", function () { prbImg(this) });
 		}
 
+
+
+
+
 		//Ecriture contenu textuel HTML dans balise "div" class "card"
 		bdCard.innerHTML =
 			'<img class="imgBd btn" id="img-' + key + '"src="' + src + '"/>' +
@@ -70,21 +95,194 @@ function showAlbumsListBd() {
 			'<li><h6 class="card-body bdTitleInfo"><strong>' + "Auteur : " + "&nbsp;" + '</strong>' + auteur.nom + '</h6></li>' +
 			'<li><h6 class="card-body bdTitleInfo"><strong>' + "Prix : " + "&nbsp;" + '</strong>' + album.prix + "€" + '</h6></li>' +
 			'</ul>' +
-			'<button id="btnAjoutBD-' + key + '" class="btnAjoutPanier btn">Ajoutez au panier</button>';
+			'<button id="btnAjoutPanierBd-' + key + '" class="btnAjoutPanier btn">Ajoutez au panier</button>';
 
 		//Ajout contenu textuel HTML dans balise "div" class "card"
 		elementListBd.appendChild(bdCard);
 
+
+
+		//Affichage ouvrage par auteur via fonction "chargeByAuthor"
+		// document.getElementById('titre-' + key).addEventListener("change",function () {showAlbumsListBd()});
+
+
+
+
+		// 		selectListSearch.addEventListener("change", function () { 
+		// 	if (selectListSearch.options[selectListSearch.selectedIndex].innerText != "Sélectionnez") {
+
+		// 		// console.log("pas super");
+
+
+		// 		//Affichage ouvrage par auteur via fonction "chargeByAuthor"
+		// 		document.getElementById('titre-' + key).addEventListener("change",function () {showAlbumsListBd()});
+
+
+		// 			//Affichage ouvrage par auteur via fonction "chargeByAuthor"
+		// 		// document.value('auteur-' + _album.idAuteur).addEventListener("change", function () { showAlbumsListBd() });
+
+
+
+		// 		//Affichage ouvrage par auteur via fonction "chargeByAuthor"
+		// 		// document.value('serie-' + _album.idSerie).addEventListener("load", function () { showAlbumsListBd() });
+		// 	} else {
+
+		// 		console.log("super");
+
+		// 		//Ajout contenu textuel HTML dans balise "div" class "card"
+		// 		elementListBd.appendChild(bdCard);
+		// 	}
+		// });
+
+
+
+
+
+
 		//Chargement + affichage auto liste auteurs, titres, séries album BD via fonction "showAlbumsBd"
 		selectListSearch.addEventListener("click", function () { showListBd(key, auteur, serie) });
 
-		//Ajout album BD dans panier achat via fonction "ajoutPanier"
-		document.getElementById('btnAjoutBD-' + key).addEventListener("click", function () { ajoutPanier(key, auteur, serie) });
-
 		//affiche fenêtre Modal via fonction "afficheModal"
 		document.getElementById('img-' + key).addEventListener("click", function () { afficheModal(key, auteur, serie) });
+
+
+
+
+
+		//si                   alors
+		if (document.getElementById('btnAjoutPanierBd-' + key) != false) {
+
+			//Ajout album BD dans panier achat via fonction "ajoutPanier"
+			document.getElementById('btnAjoutPanierBd-' + key).addEventListener("click", function () { ajoutPanier(key, auteur, serie) });
+
+			//Affiche augmentation quantité panier via click bouton "ajout panier"
+			document.getElementById('btnAjoutPanierBd-' + key).addEventListener("click", () => {
+
+				// Variable locale
+				var quantiteBd = document.getElementById('quantiteBd-' + key); //sélection input quantité BD card associée
+
+				a++; //incrémentation quantité input panier achat de +1
+
+				//si quantité <10 alors mise en forme chiffrage au format "xx" sinon "x"
+				a = (a < 10) ? "0" + a : a;
+				// console.log(a); //affiche dev
+
+				//Ajout contenu textuel quantité dans input quantite panier
+				quantitePanier.value = a;
+
+
+
+				
+
+				//Ajout contenu textuel quantité dans input quantite BD
+
+				quantiteBd.value = a;
+
+
+
+
+
+
+				// //Affiche augmentation quantité BD via click bouton "+"
+				// document.getElementById('btnPlusBd-' + key).addEventListener("click", () => {
+
+
+				// 	var quantiteBd = document.getElementById('quantiteBd-' + key); //sélection input quantité BD card associée
+
+
+				// 	a++; //incrémentation quantité input BD de +1
+
+				// 	a = (a < 10) ? "0" + a : a; //si quantité BD <10 alors mise en forme chiffrage au format "xx" sinon "x"
+				// 	// console.log(a); //affiche dev
+
+				// 	//Ajout contenu textuel quantité dans input "quantiteBd"
+				// 	quantiteBd.value = a;
+				// });
+
+
+
+
+
+
+				// if (quantitePanier.value != 0) {
+
+
+				// 	var quantiteBd = document.getElementById('quantiteBd-' + key); //sélection input quantité BD card associée
+
+				// 	//Ajout contenu textuel quantité dans input "quantiteBd"
+				// 	quantiteBd.value = a;
+				// }
+
+
+
+
+				// if (quantitePanier.value != 0) {
+				// 	//Affiche augmentation quantité BD via click bouton "+"
+				// 	document.getElementById('btnPlusBd-' + key).addEventListener("click", () => {
+
+
+				// 		var quantiteBd = document.getElementById('quantiteBd-' + key); //sélection input quantité BD card associée
+
+
+				// 		a++; //incrémentation quantité input BD de +1
+
+				// 		a = (a < 10) ? "0" + a : a; //si quantité BD <10 alors mise en forme chiffrage au format "xx" sinon "x"
+				// 		// console.log(a); //affiche dev
+
+				// 		//Ajout contenu textuel quantité dans input "quantiteBd"
+				// 		quantiteBd.value = a;
+				// 	});
+				// }
+
+
+
+
+
+			});
+
+
+
+
+
+
+
+
+
+
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// //Affiche augmentation quantité BD via click bouton "+"
+		// document.getElementById('btnMoinsBd-' + key).addEventListener("click", () => {
+		// 	a--; //décrémentation quantité input BD de -1
+
+		// 	a = (a < 10) ? "0" + a : a; //si quantité BD <10 alors mise en forme chiffrage au format "xx" sinon "x"
+		// 	// console.log(a); //affiche dev
+
+		// 	//Retrait contenu textuel quantité dans input "quantiteBd"
+		// 	quantiteBd.value = a;
+		// });
+
+
 	});
 }
+
+
+
+
 
 
 //affiche Modal BD
@@ -92,9 +290,9 @@ function afficheModal(_key, _auteur, _serie) {
 	_album = albums.get(_key); //sélection album par rapport numéro clef dans fichier "albums.js"
 	_serie = series.get(_album.idSerie); //sélection serie BD dans fichier "series.js"
 	_auteur = auteurs.get(_album.idAuteur); //sélection auteur BD dans fichier "auteurs.js"
-	// console.log(_key);
-	// console.log(_auteur.nom);
-	// console.log(_serie.nom);
+	// console.log(_key); //affiche dev
+	// console.log(_auteur.nom); //affiche dev
+	// console.log(_serie.nom); //affiche dev
 
 	//Ecriture contenu textuel HTML dans balise "h4" Modal
 	document.getElementById('titleModal').innerHTML = '<strong>' + _album.titre + '</strong>';
@@ -119,46 +317,46 @@ function prbImg(_imgDefault) {
 
 //Affichage liste titres album BD à partir fichier data "albums"
 function showListBdTitle() {
-	// console.log('btn titre activé');
+	// console.log('btn titre activé'); //affiche dev
 	btnRadioAuthor.checked = false; //désélection bouton radio "auteur"
 	btnRadioSerie.checked = false; //désélection bouton radio "série"
 
 	//Vide et ajoute contenu textuel HTML dans select
 	selectListSearch.innerHTML = "";
-	selectListSearch.innerHTML = '<option value="selection">Sélectionnez</option>';
+	selectListSearch.innerHTML = '<option id="selection" value="selection">Sélectionnez</option>';
 
 	//Chargement + affichage auto liste auteurs, titres, séries album BD via fonction "showAlbumsBd"
-	selectListSearch.addEventListener("load", function () { showListBd(_key, auteur, _serie) });
+	// selectListSearch.addEventListener("load", function () { showListBd(_key, auteur, _serie) });
 }
 
 
 //Affichage liste auteurs album BD à partir fichier data "albums, auteurs"
 function showListBdAuthor() {
-	// console.log('btn auteur activé');
+	// console.log('btn auteur activé'); //affiche dev
 	btnRadioTitle.checked = false; //désélection bouton radio "titre"
 	btnRadioSerie.checked = false; //désélection bouton radio "série"
 
 	//Vide et ajoute contenu textuel HTML dans select
 	selectListSearch.innerHTML = "";
-	selectListSearch.innerHTML = '<option value="selection">Sélectionnez</option>';
+	selectListSearch.innerHTML = '<option id="selection" value="selection">Sélectionnez</option>';
 
 	//Chargement + affichage auto liste auteurs, titres, séries album BD via fonction "showAlbumsBd"
-	selectListSearch.addEventListener("load", function () { showAListBd(_key, _auteur, _serie) });
+	// selectListSearch.addEventListener("load", function () { showAListBd(_key, _auteur, _serie) });
 }
 
 
 //Affichage liste séries album BD à partir fichier data "albums, series"
 function showListBdSerie() {
-	// console.log('btn série activé');
+	// console.log('btn série activé'); //affcihe dev
 	btnRadioTitle.checked = false; //désélection bouton radio "titre"
 	btnRadioAuthor.checked = false; //désélection bouton radio "auteur"
 
 	//Vide et ajoute contenu textuel HTML dans select
 	selectListSearch.innerHTML = "";
-	selectListSearch.innerHTML = '<option value="selection">Sélectionnez</option>';
+	selectListSearch.innerHTML = '<option id="selection" value="selection">Sélectionnez</option>';
 
 	//Chargement + affichage auto liste auteurs album BD via fonction "showAlbumsAutor"
-	selectListSearch.addEventListener("load", function () { showListBd(_key, _auteur, _serie) });
+	// selectListSearch.addEventListener("load", function () { showListBd(_key, _auteur, _serie) });
 }
 
 
@@ -173,13 +371,14 @@ function showListBd(_key, _auteur, _serie) {
 	switch (selectListSearch != false) {
 		//si sélection bouton radio "série" alors
 		case btnRadioSerie.checked == true:
-			// console.log(_serie.nom);
+			// console.log(_serie.nom); //affiche dev
 
 			//Variable locale
 			var optionSelect = document.createElement('option'); //création balise "option" pour select list
 
-			//Création attribu value "serie-" pour select list
+			//Création attribu id + value "serie-" pour select list
 			optionSelect.setAttribute('value', 'serie-' + _album.idSerie);
+			optionSelect.setAttribute('id', 'serie-' + _album.idSerie);
 
 			//Ecriture contenu textuel HTML dans balise "option" value "serie-"
 			optionSelect.innerHTML = _serie.nom;
@@ -191,13 +390,14 @@ function showListBd(_key, _auteur, _serie) {
 
 		//si sélection bouton radio "titre" alors
 		case btnRadioTitle.checked == true:
-			// console.log(_key);
+			// console.log(_key); //affiche dev
 
 			//Variable locale
 			var optionSelect = document.createElement('option'); //création balise "option" pour select list
 
-			//Création attribu value "titre-" pour select list
+			//Création attribu id + value "titre-" pour select list
 			optionSelect.setAttribute('value', 'titre-' + _key);
+			optionSelect.setAttribute('id', 'titre-' + _key);
 
 			//Ecriture contenu textuel HTML dans balise "option" value "titre-"
 			optionSelect.innerHTML = _album.titre;
@@ -209,30 +409,20 @@ function showListBd(_key, _auteur, _serie) {
 
 		//si sélection bouton radio "auteur" alors
 		case btnRadioAuthor.checked == true:
-			// console.log(_auteur.nom);
+			// console.log(_auteur.nom); //affiche dev
 
 			//Variable locale
 			var optionSelect = document.createElement('option'); //création balise "option" pour select list
 
-			//Création attribu value "Auteur-" pour select list
+			//Création attribu id + value "Auteur-" pour select list
 			optionSelect.setAttribute('value', 'auteur-' + _album.idAuteur);
+			optionSelect.setAttribute('id', 'auteur-' + _album.idAuteur);
 
 			//Ecriture contenu textuel HTML dans balise "option" value "Auteur-"
 			optionSelect.innerHTML = _auteur.nom;
 
 			//Ajout contenu textuel HTML dans balise "option" value "Auteur-"
 			selectListSearch.appendChild(optionSelect);
-
-
-
-
-
-
-			//Affichage ouvrage par auteur via fonction "chargeByAuthor"
-			selectListSearch.addEventListener("change", chargeByAuthor(_key,_auteur,_serie)); //deuxième solution écriture
-
-
-
 
 			break; //fin sélection
 
@@ -245,13 +435,13 @@ function showListBd(_key, _auteur, _serie) {
 
 //ajout album BD panier
 function ajoutPanier(_key, _auteur, _serie) {
+	//Retrait affichage titre liste panier achat 
+	titreListPanierAchat.style.display = "flex";
+
 	//Sélection fichier data "albums, auteurs, series"
 	_album = albums.get(_key); //sélection album par rapport numéro clef dans fichier "albums.js"
 	_auteur = auteurs.get(_album.idAuteur); //sélection auteur BD dans fichier "auteurs.js"
 	_serie = series.get(_album.idSerie); //sélection serie BD dans fichier "series.js"
-	// console.log(_key);
-	// console.log(_auteur.nom);
-	// console.log(_serie.nom);
 
 	//Variables locales
 	var elementListComicsBd = document.getElementById('listPaniercomicsBD'); //sélection id balise "div" HTML "Liste et panier achats"
@@ -262,6 +452,12 @@ function ajoutPanier(_key, _auteur, _serie) {
 
 	//Création attribu class "card" pour album BD
 	bdCardPanier.setAttribute('class', 'card');
+
+
+
+
+
+
 
 	//Insertion images dans balise "div" class "card"
 	//si clef album existant alors
@@ -274,6 +470,11 @@ function ajoutPanier(_key, _auteur, _serie) {
 		imgsBd[i].addEventListener("error", function () { prbImg(this) });
 	}
 
+
+
+
+
+
 	//Ecriture contenu textuel HTML dans balise "div" class "card"
 	bdCardPanier.innerHTML =
 		'<img class="imgBd btn" id="img-' + _key + '"src="' + src + '"/>' +
@@ -281,12 +482,15 @@ function ajoutPanier(_key, _auteur, _serie) {
 		'<li><h3 class="card-body bdTitleInfo"><strong>' + _album.titre + '</strong></h3></li>' +
 		'<li><h6 class="card-body bdTitleInfo"><strong>' + "Prix : " + "&nbsp;" + '</strong>' + _album.prix + "€" + '</h6></li>' +
 		'</ul>' +
-		'<button id="btnAjoutBd-' + _key + '" class="btnPlusPanier btn">+</button>' +
-		'<input type="text" id="quantiteBd-" readonly value="0">' +
-		'<button id="btnMoinsBd-' + _key + '" class="btnMoinsPanier btn">-</button>';
+		'<button id="btnMoinsBd-' + _key + '" class="btnMoinsPanier btn">-</button>' +
+		'<input type="text" id="quantiteBd-' + _key + '" readonly value="0">' +
+		'<button id="btnPlusBd-' + _key + '" class="btnPlusPanier btn">+</button>';
 
 	//Ajout contenu textuel HTML dans balise "div" class "card"
 	elementListComicsBd.appendChild(bdCardPanier);
+
+	//affiche fenêtre Modal via fonction "afficheModal"
+	document.getElementById('img-' + _key).addEventListener("click", function () { afficheModal(_key, _auteur, _serie) });
 }
 
 
@@ -296,41 +500,44 @@ function ajoutPanier(_key, _auteur, _serie) {
 
 
 
-//Affichage ouvrage par auteur
-function chargeByAuthor(_key,_auteur,_serie) {
-	//Sélection fichier data "albums, auteurs, series"
-	_album = albums.get(_key); //sélection album par rapport numéro clef dans fichier "albums.js"
-	_auteur = auteurs.get(_album.idAuteur); //sélection auteur BD dans fichier "auteurs.js"
-	_serie = series.get(_album.idSerie); //sélection serie BD dans fichier "series.js"
 
-	// var elementCategorie = document.getElementById('categoriesList'); //'variable locale' sélection liste cathégories
-	// var elementAuthor = document.getElementById('authorsList'); //'variable locale' sélection liste auteurs
-	var choiceAuthors = selectListSearch.options[selectListSearch.selectedIndex].innerText; //'variable locale' sélection contenu liste
-	var booksListByAuthors = new Array(); //'variable locale' tableau liste ouvrages par auteurs
 
-	//sélectionne contenu vide
-	// elementCategorie.options[elementCategorie.selectedIndex].innerText = "";
 
-	//si contenu liste auteurs vide alors
-	if (choiceAuthors == "") {
-		showAlbumsListBd(_album.idAuteur); //sélectionne ouvrages via fonction "showBooks"
-	}
-	//sinon si contenu liste auteurs pas vide alors
-	else {
-		//pour ouvrages allant de 0 à yy alors
-		for (var y = 0; y < _album.idAuteur.length; y++) {
-			var bookByAuthor = _album.idAuteur[y]; //'variable locale' tableau liste ouvrages par auteur
-			console.log(bookByAuthor);
-			//si présence ouvrages auteur alors
-			if (bookByAuthor_auteur.nom.indexOf(choiceAuthors) != -1) {
-				booksListByAuthors.push(bookByAuthor); //incrémente ouvrages par auteur dans tableau
-			}
-		}
-	}
+// //Affichage ouvrage par auteur
+// function chargeByAuthor(_key,_auteur,_serie) {
+// 	//Sélection fichier data "albums, auteurs, series"
+// 	_album = albums.get(_key); //sélection album par rapport numéro clef dans fichier "albums.js"
+// 	_auteur = auteurs.get(_album.idAuteur); //sélection auteur BD dans fichier "auteurs.js"
+// 	_serie = series.get(_album.idSerie); //sélection serie BD dans fichier "series.js"
 
-	//tri alphabétiquement ouvrages de la liste
-	booksListByAuthors.sort();
+// 	// var elementCategorie = document.getElementById('categoriesList'); //'variable locale' sélection liste cathégories
+// 	// var elementAuthor = document.getElementById('authorsList'); //'variable locale' sélection liste auteurs
+// 	var choiceAuthors = selectListSearch.options[selectListSearch.selectedIndex].innerText; //'variable locale' sélection contenu liste
+// 	var booksListByAuthors = new Array(); //'variable locale' tableau liste ouvrages par auteurs
 
-	//sélectionne ouvrages triés via fonction "showBooks"
-	showAlbumsListBd(booksListByAuthors);
-}
+// 	//sélectionne contenu vide
+// 	// elementCategorie.options[elementCategorie.selectedIndex].innerText = "";
+
+// 	//si contenu liste auteurs vide alors
+// 	if (choiceAuthors == "") {
+// 		showAlbumsListBd(_album.idAuteur); //sélectionne ouvrages via fonction "showBooks"
+// 	}
+// 	//sinon si contenu liste auteurs pas vide alors
+// 	else {
+// 		//pour ouvrages allant de 0 à yy alors
+// 		for (var y = 0; y < _album.idAuteur.length; y++) {
+// 			var bookByAuthor = _album.idAuteur[y]; //'variable locale' tableau liste ouvrages par auteur
+// 			console.log(bookByAuthor);
+// 			//si présence ouvrages auteur alors
+// 			if (bookByAuthor_auteur.nom.indexOf(choiceAuthors) != -1) {
+// 				booksListByAuthors.push(bookByAuthor); //incrémente ouvrages par auteur dans tableau
+// 			}
+// 		}
+// 	}
+
+// 	//tri alphabétiquement ouvrages de la liste
+// 	booksListByAuthors.sort();
+
+// 	//sélectionne ouvrages triés via fonction "showBooks"
+// 	showAlbumsListBd(booksListByAuthors);
+// }
